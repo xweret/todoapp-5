3924b4797604cfb2d7b636e2a4a45f7b8a20a410
Gabriel Garofalo <gabriel695681@gmail.com>
2022-08-10 11:09:37 -0300

    constructor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, {Component} from "react";
 
+//Task App 
 
 class App extends Component {
     render() {
+    Constructor (props) {
+        super(props);
+        this.state = {
+            newItem: "",
+            items: []
+        };
+    }
+
   return (
     <div className="App">
       <div> Ingrese una nueva tarea </div>
