daf58c20bce19194da6dad68426f0f8faa1d40c7
Gabriel Garofalo <gabriel695681@gmail.com>
2022-08-10 19:06:34 -0300

    terminada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import React, {Component} from "react";
-
-//Task App 
+import React, { Component } from 'react'
 
 class App extends Component {
-    render() {
-    Constructor (props) {
-        super(props);
-        this.state = {
-            newItem: "",
-            items: []
-        };
+  constructor(props){
+    super(props)
+    this.state = {
+      newItem: "",
+      list: []
     }
+  }
 
-  return (
-    <div className="App">
-      <div> Ingrese una nueva tarea </div>
-    </div>
-  );
+  updateInput(key,value){
+    this.setState({
+      [key]:value
+    })
+  }
+
+  addItem(){
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    const list = [...this.state.list]
+
+    list.push(newItem)
+
+    this.setState({
+      list,
+      newItem: ""
+    })
+  }
+
+  deleteItems(id){
+    const list = [...this.state.list]
+    const updatedList = list.filter(item => item.id !==id);
+
+    this.setState({list: updatedList})
+  }
+  
+  render() {
+    return (
+      <div className='app' >
+          <div>
+    
+          Add an item to the task list  
+          
+          <br/>
+
+          <input
+            type='text'
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}
+            placeholder='Add an item'
+          />
+          <button onClick={()=> this.addItem()}>
+            Add 
+            </button>
+          <br/>
+          <ul>
+            {this.state.list.map(item => {
+              return(
+                <li key={item.id}>
+                  {item.value}
+                  <button
+                  onClick={()=> this.deleteItems(item.id)}>
+                  x  
+                  </button>
+                </li>
+              )
+            })}
+          </ul>
+          </div>
+        </div>
+    )
+  }
 }
 
-}
-export default App;
+export default App
